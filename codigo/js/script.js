4f9e6b5bd009d62cba9cc6c0e9a1948edaa93ce1
ChaconErik <erickdanielchaconmedina@gmail.com>
2020-10-30 22:30:01 -0600

    cuento

---
+++ codigo/js/script.js
@@ -0,0 +1,17 @@
+console.log(5);
+console.log("Hola Mundo! ".repeat(3));
+console.log("Hola Mundo!".toLowerCase());
+console.log("Hola Mundo!");
+console.log("Hola Mundo!")
+console.log(234,456);
+console.log(2+3);
+console.log(34-17);
+//Tipos de Datos:Numericos, String, Boolean, Null, Undefined, Symbol, Object
+//Variables
+var x = 10;
+x = 'Hola';
+let y = 15;
+const vacio = false;
+/*
+Comentarios
+*/
